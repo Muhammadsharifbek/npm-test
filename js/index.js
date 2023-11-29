bcd492a8b18e5b58cbfeb6f71a1ec053a03bf548
Muhammadsharifbek <sharifahmadov690@gmail.com>
2023-11-29 14:14:01 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,19 @@
+import "../scss/main.scss";
+import "bootstrap/dist/css/bootstrap.css";
+
+import "./modules/axios";
+import axios from "axios";
+import upcomingMovies from "./modules/upcomingMovies";
+
+const getUpcomingMovies = async () => {
+  const { data } = await axios.get("/movie/upcoming", {
+    params: {
+      page: 2,
+    },
+  });
+  upcomingMovies(data.results);
+};
+
+getUpcomingMovies();
+
+console.log(1);
